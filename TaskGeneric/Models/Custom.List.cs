af70f8f99f06e37089b3bc543b1085553a460ee5
Anar Gandayev <tu7bxfga8@aztu.local>
2022-03-30 14:39:31 +0400

    commit

---
+++ TaskGeneric/Models/Custom.List.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskGeneric.Models
+{
+    class CustomList<T>
+    {
+        private T[] _list;
+        public CustomList()
+        {
+            _list = new T[0];
+        }
+        public CustomList(params T[] nums)
+        {
+            _list = new T[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                _list[i] = nums[i];
+            }
+        }
+        public void Add(T input)
+        {
+            Array.Resize(ref _list, _list.Length + 1);
+            _list[_list.Length - 1] = input;
+        }
+        public void Show()
+        {
+            foreach (var item in _list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void reverse()
+        {
+            for (int i = _list.Length-1; i >=0; i--)
+            {
+                Console.WriteLine(_list[i]);
+            }
+        }
+        public int LastIndexOf(T input)
+        {
+            for (int i = _list.Length-1; i >=0; i--)
+            {
+                if (_list[i].Equals(input))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+    class Person 
+    {
+        
+        public  int Id { get; set; }
+        public Person()
+        { 
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Id: {Id}");
+        }
+    }
+    class Student : Person 
+    {
+       
+
+        public string Name { get; set; }
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}");
+        }
+    }
+}
---
+++ TaskGeneric/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskGeneric.Models;
+
+namespace TaskGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.BackgroundColor
+             = ConsoleColor.Green;
+            Person intList = new Person();
+            Person intList1 = new Person();
+            intList.Id = 345;
+            intList1.Id = 123;
+            intList.Show();
+            intList1.Show();
+            Student stringList = new Student();
+            Student stringList1 = new Student();
+            stringList.Name ="Anar";
+            stringList1.Name = "Atar";
+            stringList.Show();
+            stringList1.Show();
+            
+            Console.WriteLine("----------------------");
+            Console.BackgroundColor
+             = ConsoleColor.Blue; 
+
+            List<int> numbers = new List<int>();
+            numbers.Add(10);
+            numbers.Add(20);
+            numbers.Add(30);
+            numbers.Add(70);
+            numbers.Add(500);    
+            numbers.Add(400);
+            numbers.Add(80);
+            numbers.Add(58);
+            Console.WriteLine("----------");
+            Console.WriteLine(numbers.LastIndexOf(80));
+            Console.WriteLine(numbers.LastIndexOf(400));
+           
+            Console.WriteLine("----------");
+
+            numbers.Remove(50);
+            numbers.Exists(numbers => numbers == 500);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            //numbers.Clear();
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine($"Count: {numbers.Count}");
+            Console.BackgroundColor
+             = ConsoleColor.Black;
+
+        }
+    }
+}
